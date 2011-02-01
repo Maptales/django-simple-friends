5e2db27b88c9f51de5942d1c0a94aea80150e52c
Philipp Wassibauer <phil@maptales.com>
2011-02-01 01:32:09 +0100

    bumped version

--- friends/__init__.py
+++ friends/__init__.py
@@ -4,7 +4,7 @@ __credits__ = [u'Atamert gen']
 
 
 __license__ = 'BSD'
-__version__ = '0.5'
+__version__ = '0.5.1'
 __maintainer__ = u'Atamert gen'
 __email__ = 'muhuk@muhuk.com'
 __status__ = 'Production'
